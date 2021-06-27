f9cf762b8cbda47646bc2b4e5516d2fc89c3542c
Kerwin Valverde <kerwin.20@gmail.com>
2021-06-26 20:02:42 -0500

    Desarrollo (#1)
    * Creacion de carpeta Dtos
    
    * Se agregaron campos a PersonaDto

---
+++ TestCreacionProyecto/Dtos/PersonaDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCreacionProyecto.Dtos
+{
+    public class PersonaDto
+    {
+        public int nIdPersona { get; set; }
+        public string nTipoDocumento { get; set; }
+        public string cNumeroDocumento { get; set; }
+        public string cNombreCompleto { get; set; }
+
+    }
+}
