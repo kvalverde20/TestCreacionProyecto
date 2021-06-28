2a9655c22741e7914e1b5b48008e7fde73c672ac
KerwinVR <kerwin.20@gmail.com>
2021-06-28 15:47:40 -0500

    Se eliminó campos de auditoria

--- TestCreacionProyecto/Dtos/GrupoDto.cs
+++ TestCreacionProyecto/Dtos/GrupoDto.cs
@@ -16,9 +16,5 @@ namespace TestCreacionProyecto.Dtos
         public string Horario { get; set; }
         public string TipoGrupo { get; set; }
         public bool? Estado { get; set; }
-        public string UsuarioCreacion { get; set; }
-        public DateTime? FechaCreacion { get; set; }
-        public string UsuarioActualizacion { get; set; }
-        public DateTime? FechaActualizacion { get; set; }
     }
 }
