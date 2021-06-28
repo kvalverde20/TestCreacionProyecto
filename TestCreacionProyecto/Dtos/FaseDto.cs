37d0732fec5b07e6398dc7a17e64021a1d6a8059
devker <kervalverder@gmail.com>
2021-06-28 10:52:10 -0500

    Se agrega FaseDto.cs

---
+++ TestCreacionProyecto/Dtos/FaseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCreacionProyecto.Dtos
+{
+    public class FaseDto
+    {
+        public int IdFase { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int? Duracion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string UsuarioActualizacion { get; set; }
+        public DateTime? FechaActualizacion { get; set; }
+    }
+}
