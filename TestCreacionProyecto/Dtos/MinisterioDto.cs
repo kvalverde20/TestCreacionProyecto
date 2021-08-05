b40890adf477574d21f5a56f767612f949d623c8
KerwinVR <kerwin.20@gmail.com>
2021-08-05 11:57:49 -0500

    Se agregó MinisterioDto

---
+++ TestCreacionProyecto/Dtos/MinisterioDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCreacionProyecto.Dtos
+{
+    public class MinisterioDto
+    {
+        public int idMinistro { get; set; }
+    }
+}
